27d5bc9e5cea3245c8ccb1fd75c79e851a89ac55
macbook <macbook@MacBook-Pro.local>
2024-11-21 17:52:19 -0800

    Search words by condition

--- self.py
+++ self.py
@@ -1,3 +1,17 @@
-print ('new branch')
+# The program has a line `string`.
+# Print the all words, containing the letter 'o' (in any case (upper/lower)) twice, as a title.
+
+string = "This tool is cool. But that owl is awful. MAGIC TOOLS Ltd."
+
+list_words = string.split(' ')
+
+result = []
+
+for word in list_words:
+    if word.find('oo') != -1  or word.find('OO') != -1:
+        result.append(word)
+formatted_result = " ".join(word.capitalize() for word in result)
+print('Result: ' + formatted_result)
+
 
 
