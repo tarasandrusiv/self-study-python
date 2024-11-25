119d990322a0c98abdc00a2e13cafc92bf917bf1
macbook <macbook@MacBook-Pro.local>
2024-11-24 22:26:13 -0800

    Adding averages to the list

---
+++ Hometask 16.py
@@ -0,0 +1,10 @@
+# There is a list of at least two float elements.
+# Create a new list. It should contain elements of initial list, and between them should be added average of that elements.
+
+lst = [1, 2, 3, 4, 6]
+
+new_lst = [(lst[i] + lst[i + 1]) / 2 for i in range(1, len(lst) - 1)]
+result = new_lst + lst
+sorted_result = sorted(result)
+print(new_lst)
+print(sorted_result)
