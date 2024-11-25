c6a671fa6806da48db3971d90bdbf7ebfaaa9f44
macbook <macbook@MacBook-Pro.local>
2024-11-24 22:31:47 -0800

    update h16

--- Hometask 16.py
+++ Hometask 16.py
@@ -3,7 +3,7 @@
 
 lst = [1, 2, 3, 4, 6]
 
-new_lst = [(lst[i] + lst[i + 1]) / 2 for i in range(1, len(lst) - 1)]
+new_lst = [(lst[i] + lst[i + 1]) / 2 for i in range(len(lst) - 1)]
 result = new_lst + lst
 sorted_result = sorted(result)
 print(new_lst)
