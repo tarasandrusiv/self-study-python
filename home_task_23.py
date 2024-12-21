89a33a3276de2c2eaa9847381e1ca462ecb5df9a
macbook <macbook@MacBook-Pro.local>
2024-12-20 22:30:07 -0800

    Second largest number in the list

--- home_task_23.py
+++ home_task_23.py
@@ -1,10 +1,8 @@
-# Write function that takes list of numbers, and returns the second largest number.
-# If there's no second largest number in the list (empty list is passed, or list contains only the same number), function should return None.
-# You should NOT use max/min built-n functions, sort/sorted.
-# Write implementation that performs only one pass through the list (complexity O(n)).
-# second_largest_number([])  # None
-# second_largest_number([1, 1])  # None
-# second_largest_number([1, 2, 3, 4, 5])  # 4
+'''Write function that takes list of numbers, and returns the second largest number.
+If there's no second largest number in the list (empty list is passed, or list contains only the same number), function should return None.
+You should NOT use max/min built-n functions, sort/sorted.
+Write implementation that performs only one pass through the list (complexity O(n)).'''
+from operator import index
 
 
 def second_largest_number(lst):
@@ -13,20 +11,15 @@ def second_largest_number(lst):
     for number in range(len(lst)):
         if number == len(lst) - 1:
             break
-        elif lst[number] > lst[number + 1]:
+        if lst[number] > lst[number + 1] and lst[number] >= max_number:
             max_number = lst[number]
         else:
-            max_number = lst[number + 1]
-            lesser_number = lst[number]
+            if lst[number + 1] > max_number:
+                max_number = lst[number + 1]
+            if lst[number] > lesser_number or index(lst[number]) == len(lst):
+                lesser_number = lst[number]
+            else:
+                continue
     print(max_number)
     print(lesser_number)
-
-second_largest_number([2, 1, 3, 4, 5, 45, 87, 555555, 6])
-
-# my_list = [10, 20, 30, 40]
-#
-# # Use the range to track the current and next index
-# for i in range(len(my_list)):
-#     current_item = my_list[i]
-#     next_item = my_list[i + 1] if i + 1 < len(my_list) else None
-#     print(f"Current: {current_item}, Next: {next_item}")
+second_largest_number([2, 1, 3, 4, 5, 45, 87, 555555, 6, 77999, 0, 32, 2, 33333333333, 99999999999999, 453, 0])
