97b380b07bfdf364f51dc5de36bf3c7e165beb0e
macbook <macbook@MacBook-Pro.local>
2024-12-20 23:06:03 -0800

    Second largest number in the list upd

--- home_task_23.py
+++ home_task_23.py
@@ -8,18 +8,25 @@ from operator import index
 def second_largest_number(lst):
     max_number = 0
     lesser_number = 0
-    for number in range(len(lst)):
-        if number == len(lst) - 1:
-            break
-        if lst[number] > lst[number + 1] and lst[number] >= max_number:
-            max_number = lst[number]
-        else:
-            if lst[number + 1] > max_number:
-                max_number = lst[number + 1]
-            if lst[number] > lesser_number or index(lst[number]) == len(lst):
-                lesser_number = lst[number]
+    if len(lst) == 0:
+        lesser_number = None
+    else:
+        for number in range(len(lst)):
+            if number == len(lst) - 1:
+                break
+            if lst[number] > lst[number + 1] and lst[number] >= max_number:
+                max_number = lst[number]
             else:
-                continue
+                if lst[number + 1] > max_number:
+                    lesser_number = max_number
+                    max_number = lst[number + 1]
+                if lst[number] > lesser_number or index(lst[number]) == len(lst):
+                    lesser_number = lst[number]
+                else:
+                    continue
+        if max_number == lesser_number:
+            lesser_number = None
+
     print(max_number)
     print(lesser_number)
-second_largest_number([2, 1, 3, 4, 5, 45, 87, 555555, 6, 77999, 0, 32, 2, 33333333333, 99999999999999, 453, 0])
+second_largest_number([2, 1, 3, 4, 5, 45, 87, 555555, 6, 77999, 0, 32, 2, 33333333333, 99999999999999])
