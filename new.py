e30be9d7284c44c217a177d012726adc5a1b3380
macbook <macbook@MacBook-Pro.local>
2025-08-31 08:16:38 -0700

    decorator skip_if

--- new.py
+++ new.py
@@ -1,3 +1,22 @@
-print('hello Taras')
+# dates = ['1-Jan', '25-Dec', '12-Feb', '03-Jan', '30-Nov']
+#
+# sorted_dates = sorted(
+#     dates,
+#     key=lambda d: (
+#         ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'].index(d.split('-')[1]),
+#         int(d.split('-')[0])
+#     )
+# )
+#
+# print(sorted_dates)
+#
+#
+# a = ['dd', 'dsd', 'dsw']
+#
+# print(a.index(a.split('-')))
 
+import pytest
+def test_two_plus_two():
+   assert 2 + 2 == 5
 
+test_two_plus_two()
